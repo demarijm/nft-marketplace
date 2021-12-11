28c4728d1815646c19271456301b642c9b4b1831
demarijm <demarijmiller@gmail.com>
2021-12-11 04:11:14 -0500

    Good pauing point before adding more

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
-
-const pId = process.env.projectId
-
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim() || "";
+const privateId = fs.readFileSync(".projectId").toString().trim() || "";
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mumbai: "https://polygon-mumbai.infura.io/v3/" + {pId},
-    accounts: []
-  },
-  mainnet: {
-      url: "https://polygon-mainnet.infura.io/v3/${pId}",
-      accounts: []
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${privateId}`,
+      accounts: [privateKey]
     },
-  solidity: "0.8.4",
+    matic: {
+        url: `https://polygon-mainnet.infura.io/v3/${privateId}`,
+        accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
-
